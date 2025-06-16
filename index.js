4260465c7c45be6c8079475683d51fac3fc2dae0
Asma-Salah <asmaysalah2030@gmail.com>
2025-06-16 10:38:31 +0300

    latest commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/users/:id", async (req, res) => {
 app.post("/users", async (req, res) => {
   try {
     const { firstName, lastName, emailAddress, username } = req.body;
-    const createPost = await client.user.create({
+    const createPost = await client.user.createMany({
       data: {
         firstName,
         lastName,
